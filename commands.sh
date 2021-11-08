65f2b6eeeed473a486b258acf9b3453bf29f1c87
Prashant Kesarwani <47484727+pikukesar@users.noreply.github.com>
2021-11-08 13:28:56 -0500

    Create commands.sh

---
+++ commands.sh
@@ -0,0 +1,22 @@
+
+python3 -m venv ~/.flask-ml-azure
+source ~/.flask-ml-azure/bin/activate
+cd flask-ml-azure-serverless-demo
+make install
+az webapp up -n flask-ml-service-pkesarwani
+
+##OUTPUT
+{
+  "URL": "http://flask-ml-service-pkesarwani.azurewebsites.net",
+  "appserviceplan": "udacityprashants_asp_Linux_southcentralus_0",
+  "location": "southcentralus",
+  "name": "flask-ml-service-pkesarwani",
+  "os": "Linux",
+  "resourcegroup": "udacityprashants_rg_Linux_southcentralus",
+  "runtime_version": "python|3.7",
+  "runtime_version_detected": "-",
+  "sku": "PREMIUMV2",
+  "src_path": "//home//prashnat//flask-ml-azure-serverless-demo"
+}
+
+
